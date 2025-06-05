00b1891788e17a120193a50e7b61b6a9940ed5d4
Mireille05 <kelysemireille@gmail.com>
2025-06-06 00:36:45 +0200

    i did a kata that is about binary addition

---
+++ binaryAddition.js
@@ -0,0 +1,12 @@
+function addBinary(a,b) {
+  let sum = a + b;
+  return sum.toString(2)
+
+}
+
+console.log(addBinary(1 , 0));
+
+console.log(addBinary(11 , 10));
+
+
+//to.string(2) converts the decimal sum into a binary string representation

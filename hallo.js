a754fb3b985aae552cb3035f4c7cacd0d120d8b0
Mireille05 <kelysemireille@gmail.com>
2025-06-12 23:45:10 +0200

    added consoles

--- hallo.js
+++ hallo.js
@@ -4,4 +4,8 @@
   const greet = greetings.toLowerCase();
   return languages.some(word => greet.includes(word));
 }
+  console.log(validateHello('waguan'));
+  console.log(validateHello('ciao'));
+  console.log(validateHello('salut'));
+  console.log(validateHello('ahoj'));
   

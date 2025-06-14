99eb314fda9074101bd709f1be3359ae528419e9
Mireille05 <kelysemireille@gmail.com>
2025-06-15 00:06:23 +0200

    i added consoles to check how it's works

--- removeStringSpaces.js
+++ removeStringSpaces.js
@@ -1,4 +1,7 @@
 function noSpace(str) {
     return str.replace(/ /g, '');
 }
-console.log();
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+console.log(noSpace("ss j 8   mB  liB8 dffss g  imjB8B8  jl  B"))
+console.log(noSpace("8 j 8   mBliB   8g  imjB 8 B8  j l  B"))
+console.log(noSpace("K U B W A Y O"))

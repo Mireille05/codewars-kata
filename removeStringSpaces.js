e2423e36784dd0b595dd1bf00a77830b923c1c74
Mireille05 <kelysemireille@gmail.com>
2025-06-16 23:09:01 +0200

    added 2 consoles

--- removeStringSpaces.js
+++ removeStringSpaces.js
@@ -1,7 +1,7 @@
 function noSpace(str) {
     return str.replace(/ /g, '');
 }
-console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
-console.log(noSpace("ss j 8   mB  liB8 dffss g  imjB8B8  jl  B"))
-console.log(noSpace("8 j 8   mBliB   8g  imjB 8 B8  j l  B"))
+
 console.log(noSpace("K U B W A Y O"))
+console.log(noSpace("E L Y S E"))
+console.log(noSpace("M I R E I L L E"))

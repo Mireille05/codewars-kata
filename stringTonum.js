b9dcca1d84a46eecd80c622d162cab8468eb2cae
Mireille05 <kelysemireille@gmail.com>
2025-06-17 13:18:07 +0200

    a kata to change a num to string

---
+++ stringTonum.js
@@ -0,0 +1,22 @@
+// Method 1: parseInt()
+function stringToNumber(str) {
+    return parseInt(str, 10);
+}
+
+// // Method 2: Number() constructor
+// function stringToNumber(str) {
+//     return Number(str);
+// }
+
+// // Method 3: Unary plus operator
+// function stringToNumber(str) {
+//     return +str;
+// }
+
+// // Method 4: parseFloat() (works for integers too)
+// function stringToNumber(str) {
+//     return parseFloat(str);
+// }
+
+
+

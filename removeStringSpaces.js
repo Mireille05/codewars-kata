958a4b3739b25310e18111c93c1f27405322d0e3
Mireille05 <kelysemireille@gmail.com>
2025-07-03 22:35:23 +0200

    i added question link

--- removeStringSpaces.js
+++ removeStringSpaces.js
@@ -6,3 +6,7 @@ console.log(noSpace("K U B W A Y O"))
 console.log(noSpace("E L Y S E"))
 console.log(noSpace("M I R E I L L E"))
 console.log(noSpace("t o d a y "))
+
+
+
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5

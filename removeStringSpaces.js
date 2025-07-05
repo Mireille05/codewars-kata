8fb5b09512b30d2650137c1f2b69ef5346ee26f7
Mireille05 <kelysemireille@gmail.com>
2025-07-05 21:58:42 +0200

    i tried another way

--- removeStringSpaces.js
+++ removeStringSpaces.js
@@ -1,12 +1,21 @@
-function noSpace(str) {
-    return str.replace(/ /g, '');
-}
+// function noSpace(str) {
+//     return str.replace(/ /g, '');
+// }
 
-console.log(noSpace("K U B W A Y O"))
-console.log(noSpace("E L Y S E"))
-console.log(noSpace("M I R E I L L E"))
-console.log(noSpace("t o d a y "))
+// console.log(noSpace("K U B W A Y O"))
+// console.log(noSpace("E L Y S E"))
+// console.log(noSpace("M I R E I L L E"))
+// console.log(noSpace("t o d a y "))
 
 
 
 // https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+
+
+
+function noSpace(x) {
+  const y = x.split(" ").join("");
+  return y;
+}
+console.log("K U B W A Y O");

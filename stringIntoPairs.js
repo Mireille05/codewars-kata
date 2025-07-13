7742fd70adf0a6d78bdcd8289b28e63bf8b15960
Mireille05 <kelysemireille@gmail.com>
2025-07-13 21:55:17 +0200

    i added another console.log

--- stringIntoPairs.js
+++ stringIntoPairs.js
@@ -1,16 +1,17 @@
 function splitPairs(str) {
   if (str.length % 2 !== 0) {
-    str += '_'; // Append underscore if length is odd
+    str += "_"; // Append underscore if length is odd
   }
   let result = [];
-  for (let i = 0; i < str.length; i +=2) {
+  for (let i = 0; i < str.length; i += 2) {
     result.push(str.slice(i, i + 2));
   }
   return result;
 }
-console.log(splitPairs('abc'));
-console.log(splitPairs('abcdef'));
-console.log(splitPairs('ab'));
+console.log(splitPairs("abc"));
+console.log(splitPairs("abcdef"));
+console.log(splitPairs("ab"));
+console.log(splitPairs("God is goood all the time"));
 
 // i +=2 to iterate by 2 after each incrementation
 //i += 2 moves the loop forward by two characters so you don't process the same characters twice.

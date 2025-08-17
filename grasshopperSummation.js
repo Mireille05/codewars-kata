8107cb74fb31651e4b78f9410d8749d6df0710e3
Mireille05 <kelysemireille@gmail.com>
2025-08-17 16:09:27 +0200

    i added codes of for of

--- grasshopperSummation.js
+++ grasshopperSummation.js
@@ -1,10 +1,36 @@
-function summation(num) {
-  let sum = 0;
-  for (let i = 1; i <= num; i++) {
-    sum += i;
-  }
-  return sum;
+// function summation(num) {
+//   let sum = 0;
+//   for (let i = 1; i <= num; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+// console.log(summation(3));
+// console.log(summation(10));
+// console.log(summation(4));
+
+// let scores = [80, 90, 70];
+
+// for (let score of scores) {
+//   score = score + 15;
+//   console.log(score);
+// }
+
+// let colors = ["Red", "Green", "Blue"];
+
+// for (const [index, color] of colors.entries()) {
+//   console.log(`${color} is at index ${index}`);
+// }
+
+let scores = [10, 20, 30];
+scores.message = "Hi";
+
+console.log("for...in:");
+for (let score in scores) {
+  console.log(score);
 }
-console.log(summation(3));
-console.log(summation(10));
-console.log(summation(4));
+
+console.log("for...of:");
+for (let score of scores) {
+  console.log(score);
+}

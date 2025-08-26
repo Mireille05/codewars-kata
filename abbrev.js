c276bb3d30d2d4129e3b387cd9f3272e060bd101
Mireille05 <kelysemireille@gmail.com>
2025-08-26 21:00:56 +0200

    a kata about abbreviating names

---
+++ abbrev.js
@@ -0,0 +1,8 @@
+function abbrevName(name) {
+  const words = name.split(" ");
+  return `${words[0][0].toUpperCase()}.${words[1][0].toUpperCase()}`;
+}
+
+console.log(abbrevName("Sam Harris"));
+console.log(abbrevName("patrick Kaney"));
+console.log(abbrevName("Kubwayo elyse mireille"));
--- learning.js
+++ learning.js
@@ -1,5 +1,4 @@
 // JavaScript's most common use is front-end web development, improving the user interface and interactivity for websites. Application developers may also use JavaScript beyond the web,
 // to develop games, mobile applications, and server applications.
 
-
-// todaaaaaaaaaaaaaaayyyy
+//

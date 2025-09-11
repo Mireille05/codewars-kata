4cba645ce41efbf0a5624798bf7017119ea09da3
Mireille05 <kelysemireille@gmail.com>
2025-09-11 20:53:57 +0200

    i added java session

--- abbrev.js
+++ abbrev.js
@@ -13,3 +13,58 @@ console.log(abbrevName("Kaliza lyiii"));
 console.log(abbrevName("Kami lyiii"));
 
 // create simple calculator using console application
+
+// netbean 8.2
+// jdk version 8
+// i go with postgress
+// H@ck3r0i
+// Javaaa assignment       1. What is a software ,why,how
+// software and software system
+// java helps a developer to solve a problem through developing a software
+
+/*
+variable when u use it u create a storage
+variable name should have a meaningful name that describes the data it holds
+
+java syntax
+access modifier,datatype variable names
+
+how many types of access modifiers we have and how to use them (examples)
+
+variable name should start with small letters,no special characterslike $@#-,no space,no numbers
+
+name using camelCase(eg firstName) ,snake_case(first_name)
+
+how many datatypes we have in java?
+
+
+Functions /Method in java
+
+functions help to not repeat the codes
+
+ syntax for functions
+ AccessModifiers returnType methodNames{
+
+ }
+
+ #btn parameters and arguments and how to use them
+
+
+
+DRY:Don't Repeat Yourself
+
+KISS:Keep it short stupid
+
+boolean default value is false
+Boolean default value is null 
+
+
+flow controls
+conditions(if,elseif,else)
+  switch case
+  loops(forloop,whileloop,do-while,foreach)
+  breaks(break,continue) how to use them
+
+   what are operations in java(bitwise ,arithmetic,logical)
+
+*/
